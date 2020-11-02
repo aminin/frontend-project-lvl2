9ad5c2155887328cac446ae4b68345d087392235
Anton Minin <anton.a.minin@gmail.com>
2020-11-03 00:24:37 +0300

    Шаг 3. Иммутабельность

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,14 @@
 import program from 'commander';
+import genDiff from '../index.js';
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  })
 ;
 
 program.parse(process.argv);
